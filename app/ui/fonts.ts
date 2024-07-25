10d4fdeb8572492f3068f45d07a01f91a1ec2e3d
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-07-25 15:34:30 +0300

    Adding a secondary font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,8 @@
-import { Inter } from 'next/font/google'; // Import Inter font from Google Fonts
+import { Inter, Lusitana } from 'next/font/google'; // Import the fonts
  
-export const inter = Inter({ subsets: ['latin'] }); // Import Inter font with Latin subset
+export const inter = Inter({ subsets: ['latin'] }); // Define the font with the latin subset
+ 
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+}); // Define the font with the latin subset and the 400 and 700 weights
