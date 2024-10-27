d2939ae13358e4dff1a9d99131e40c089b87669e
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-10-27 11:00:06 +0300

    Rebuild: from scratch

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,54 +1,35 @@
-'use client'; // Denotes that this module is client-side code and should be bundled by Vite.
-
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
-import Link from 'next/link'; // Import the Link component from Next.js
-import { usePathname } from 'next/navigation'; // Import the usePathname hook from Next.js
-import clsx from 'clsx'; // Import the clsx utility function
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  {
-    name: 'Home',
-    href: '/dashboard',
-    icon: HomeIcon
-  },
+  { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
     name: 'Invoices',
     href: '/dashboard/invoices',
     icon: DocumentDuplicateIcon,
   },
-  {
-    name: 'Customers',
-    href: '/dashboard/customers',
-    icon: UserGroupIcon
-  },
+  { name: 'Customers', href: '/dashboard/customers', icon: UserGroupIcon },
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+          <a
             key={link.name}
             href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
-            )}
+            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link>
+          </a>
         );
       })}
     </>
--- app/ui/fonts.ts
+++
@@ -1,8 +0,0 @@
-import { Inter, Lusitana } from 'next/font/google'; // Import the fonts
- 
-export const inter = Inter({ subsets: ['latin'] }); // Define the font with the latin subset
- 
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-}); // Define the font with the latin subset and the 400 and 700 weights
