e9cd37b68085f0afd7ef525c753068d6abebd7ce
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-11-05 19:34:56 +0200

    Adding Search and Pagination with useDebouncedCallback

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,19 +2,21 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  const handleSearch = (term: string) => {
+  const handleSearch = useDebouncedCallback((term: string) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     term ? params.set('query', term) : params.delete('query')
     replace(`${pathname}?${params.toString()}`);
     console.log(term)
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
