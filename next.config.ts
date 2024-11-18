8eeca3339faf25ccfa2f840031e3bdd0b13356a2
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-11-18 20:59:16 +0200

    Added: external image domains in the next.config.js

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   // experimental: {
   //   ppr: 'incremental',
   // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // Match https protocol
+        hostname: '**',    // Match any hostname (wildcard for all domains)
+      },
+    ],
+    domains: ['avatars.githubusercontent.com','drive.google.com'], // Add the domain here
+  },
 };
 
 export default nextConfig;
