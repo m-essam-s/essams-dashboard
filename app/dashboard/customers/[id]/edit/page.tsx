7df58a275c21343493212ad6545cdcb4515b2a2d
Mohamed Essam <109111496+m-essam-s@users.noreply.github.com>
2024-11-19 14:38:28 +0200

    handle not found dynamic route segments

--- app/dashboard/customers/[id]/edit/page.tsx
+++ app/dashboard/customers/[id]/edit/page.tsx
@@ -1,11 +1,16 @@
 import Form from '@/app/ui/customers/edit-form';
 import Breadcrumbs from '@/app/ui/customers/breadcrumbs';
 import { fetchCustomerById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
     const params = await props.params;
     const id = params.id;
     const customers = await fetchCustomerById(id);
+
+    if (!customers) {
+        notFound();
+    }
 
     return (
         <main>
